52e94e3ee4479e5ea567f627fa03b4f525ded53b
Rafael Manfrim <rafaelmanfrim2004@gmail.com>
2025-04-15 17:01:35 -0300

    Instalando e Inicializando OneSignal

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import { StatusBar } from 'react-native';
 import { NativeBaseProvider } from 'native-base';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+import { OneSignal } from 'react-native-onesignal';
 
 import { Routes } from './src/routes';
 
@@ -8,6 +9,8 @@ import { THEME } from './src/theme';
 import { Loading } from './src/components/Loading';
 
 import { CartContextProvider } from './src/contexts/CartContext';
+
+OneSignal.initialize("136a2733-f04d-4fe6-ab2d-6c30bc5d51a0")
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
